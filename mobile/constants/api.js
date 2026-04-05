dbb01e8e5d1d5b0fa528464bf9a374a62c61016c
ktng8888 <keantiongng@gmail.com>
2026-04-05 20:38:40 +0800

    Tidy up the api.js & server.js

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,29 +1,3 @@
-/*
-// Change this to your computer's IP address
-// Find it by running: ipconfig (Windows) or ifconfig (Mac)
-// Must use IP not localhost — phone can't reach localhost
-//Use same WIFI for both computer and phone
-
-// Example: 'http://192.168.1.5:5000/api'
- 
-//Laptop 
-//const API_BASE_URL = 'http://10.100.100.34:5000/api';
-//export const BASE_URL = 'http://10.100.100.34:5000';
-
-//D1002 unifi
-
-const API_BASE_URL = 'http://192.168.0.50/api';
-export const BASE_URL = 'http://192.168.0.50:5000';
-
-//mobile hotspot
-/*
-const API_BASE_URL = 'http://10.58.71.170:5000/api';
-export const BASE_URL = 'http://10.58.71.170:5000';
-
-
-export default API_BASE_URL;
-*/
-
 import { Platform } from 'react-native';
 
 // D1002 unifi
@@ -31,9 +5,6 @@ const UNIFI_IP = '192.168.0.50';
 
 // Mobile hotspot
 const HOTSPOT_IP = '10.58.71.170';
-
-// Laptop
-const LAPTOP_IP = '10.100.100.34';
 
 // eBfi@MMU
 const MMU_IP = '10.121.33.212';
