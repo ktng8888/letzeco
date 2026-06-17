11d911862205bb00ae4970f0030325c4a20e4f7f
ktng8888 <keantiongng@gmail.com>
2026-06-17 20:33:21 +0800

    Update api.js

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -3,27 +3,26 @@ import { Platform } from 'react-native';
 // D1002 unifi
 const UNIFI_IP = '192.168.0.50';
 
-// Mobile hotspot
-const HOTSPOT_IP = '10.58.71.170';
-
-// eBfi@MMU
-const MMU_IP = '10.121.37.254';
-
-//Hotspot (in MMU)
-const MMU_HOTSPOT_IP = '10.143.15.170';
-
-//Hospot (in supervisor's office)
-const MMU_SUPERVISOR_IP = '10.143.15.170';
+// Hotspot (in supervisor's office)
+const HOTSPOT_IP = '10.143.15.170';
 
 // Change this to switch networks
-const NETWORK_IP = UNIFI_IP;
+const NETWORK_IP = HOTSPOT_IP;
 
-const API_BASE_URL = Platform.OS === 'web'
-  ? 'http://localhost:5000/api'
-  : `http://${NETWORK_IP}:5000/api`;
+// Backend tunnel URL (Update this if the tunnel URL changes)
+const TUNNEL_URL = 'https://passage-angel-democrat-descending.trycloudflare.com';
 
-export const BASE_URL = Platform.OS === 'web'
+// Change this to switch backend mode
+const USE_TUNNEL = true;
+
+const LOCAL_BASE_URL = Platform.OS === 'web'
   ? 'http://localhost:5000'
   : `http://${NETWORK_IP}:5000`;
+
+export const BASE_URL = USE_TUNNEL
+  ? TUNNEL_URL
+  : LOCAL_BASE_URL;
+
+const API_BASE_URL = `${BASE_URL}/api`;
 
 export default API_BASE_URL;
